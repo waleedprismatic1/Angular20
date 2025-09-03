a7a4eb1613b5951236108fec0231b18cf0c30394
waleedprismatic <waleed@prismatic-technologies.com>
2025-09-03 15:18:06 +0500

    a

--- Webapi/Webapi/obj/Debug/net8.0/Webapi.AssemblyInfo.cs
+++ Webapi/Webapi/obj/Debug/net8.0/Webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca8ee4f6b3e391bba422d239e6368598b423668")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
